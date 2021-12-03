88a63f91398df1e4dbe427ed2d2c87406f9f6edf
chi-ino <cra.00x@gmail.com>
2021-12-03 13:39:43 +0900

    redo_first commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
 
   def index
@@ -47,6 +47,9 @@ class PrototypesController < ApplicationController
   end
 
 
+  
+ 
+  
   private
 
   def prototype_params
@@ -54,10 +57,11 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
 
-  
+
 end
